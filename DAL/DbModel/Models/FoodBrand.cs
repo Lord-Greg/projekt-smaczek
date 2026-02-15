a5468f7f57eaf02d5f0fc585a88f89a4c8afd962
Greg <stepniewski.greg@gmail.com>
2026-02-15 21:31:25 +0100

    Cleanup.

--- DAL/DbModel/Models/FoodBrand.cs
+++ DAL/DbModel/Models/FoodBrand.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DbModel.Model
+﻿namespace DbModel.Model
 {
     internal class FoodBrand
     {
--- DAL/DbModel/Models/FoodProduct.cs
+++ DAL/DbModel/Models/FoodProduct.cs
@@ -1,7 +1,4 @@
 ﻿using DbModel.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DbModel.Model
 {
