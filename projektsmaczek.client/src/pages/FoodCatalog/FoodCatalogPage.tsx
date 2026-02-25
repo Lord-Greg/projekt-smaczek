048a89ada4f782e1a2f38b101cdcaf5fe58ef252
Greg <stepniewski.greg@gmail.com>
2026-02-25 02:16:39 +0100

    Prettier FoodCatalogPage.

--- projektsmaczek.client/src/pages/FoodCatalog/FoodCatalogPage.tsx
+++ projektsmaczek.client/src/pages/FoodCatalog/FoodCatalogPage.tsx
@@ -1,4 +1,4 @@
-import { Layout, Button } from "antd";
+import { Layout, Button, Flex } from "antd";
 import { Link } from 'react-router-dom';
 import FoodProductsTable from "./components/FoodProductsTable";
 
@@ -6,10 +6,13 @@ const FoodCatalogPage: React.FC = () => {
 	return (
 		<>
 			<Layout>
+				<h2>Food catalog</h2>
 				<FoodProductsTable />
-				<Link to="/FoodCatalog/AddProduct">
-					<Button type="primary" className="button-normal">Add new product</Button>
-				</Link>
+				<Flex vertical={false} justify="center" style={{ marginBottom: 15 }}>
+					<Link to="/FoodCatalog/AddProduct">
+						<Button type="primary" className="button-normal">Add new product</Button>
+					</Link>
+				</Flex>
 			</Layout>
 		</>
 	);
