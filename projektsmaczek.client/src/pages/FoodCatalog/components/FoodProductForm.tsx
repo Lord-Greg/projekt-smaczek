96ee4f24a33b3d8657fd9420d125b337a643c80e
Greg <stepniewski.greg@gmail.com>
2026-02-25 02:15:44 +0100

    Add FoodProductForm.

--- projektsmaczek.client/src/pages/FoodCatalog/components/FoodProductForm.tsx
+++ projektsmaczek.client/src/pages/FoodCatalog/components/FoodProductForm.tsx
@@ -1,16 +1,22 @@
-import { Form, Input, Select, theme } from "antd";
+import { Form, Input, Select, Button, Flex } from "antd";
+import { FoodType } from "@/models/enums/FoodType";
+import { enumToArrayReversed } from "@/utils/enum-to-array";
+import { useNavigate } from "react-router-dom";
+
 
 const FoodProductForm: React.FC = () => {
-	const {
-		token: { colorBgContainer, borderRadiusLG },
-	} = theme.useToken();
+	const navigate = useNavigate();
 
 	return (
 		<>
-			<Form>
+			<h2>New food product</h2>
+
+			<Form
+				labelCol={{ span: 7 }}
+				wrapperCol={{ span: 12 }}
+			>
 				<Form.Item label="Brand Name" name="brandName">
 					<Select
-						style={{ width: 120 }}
 						options={[
 							{ value: '2', label: 'Purina' },
 							{ value: '3', label: 'Royal Canin' },
@@ -18,6 +24,27 @@ const FoodProductForm: React.FC = () => {
 						]}
 					/>
 				</Form.Item>
+				<Form.Item label="Name" name="name">
+					<Input maxLength={500} />
+				</Form.Item>
+				<Form.Item label="Description" name="description">
+					<Input maxLength={1000} />
+				</Form.Item>
+				<Form.Item label="Food type" name="foodType">
+					<Select options={enumToArrayReversed(FoodType)} />
+				</Form.Item>
+				<Flex vertical={false} gap={"small"} justify="center">
+					<Form.Item>
+						<Button type="primary" htmlType="submit">
+							Submit
+						</Button>
+					</Form.Item>
+					<Form.Item>
+						<Button type="primary" onClick={() => navigate(-1)}>
+							Cancel
+						</Button>
+					</Form.Item>
+				</Flex>
 			</Form>
 		</>
 	);
