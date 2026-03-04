14dbff87b2658ca94e490866dd7060abf979ed4e
Greg <stepniewski.greg@gmail.com>
2026-03-04 23:50:27 +0100

    Simple repository for FoodProduct.

---
+++ DAL/DbModel/Repositories/FoodProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using DbModel.Contexts;
+using DbModel.Model;
+using DbModel.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbModel.Repositories
+{
+	internal class FoodProductRepository : IFoodProductRepository
+	{
+		private FoodContext _dbContext;
+
+		public FoodProductRepository() {
+			_dbContext = new FoodContext(new DbContextOptions<FoodContext>());
+		}
+
+		public async Task<IEnumerable<FoodProduct>> GetAllAsync()
+		{
+			return await _dbContext.FoodProducts
+				.Include(fp => fp.FoodBrand)
+				.Include(fp => fp.Items)
+				.ToListAsync();
+		}
+	}
+}
